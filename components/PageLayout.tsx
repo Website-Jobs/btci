ccc075b22e771ac50f7b25b034dcc172ac74a696
myreedbricks <reedbricks@gmail.com>
2022-09-10 17:17:14 +0100

    Fixed Js

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -4,6 +4,7 @@ import HeaderArea from "./website/HeaderArea";
 import FooterArea from "./website/FooterArea";
 import CopyRight from "./website/CopyRight";
 import NavigationBar from "./website/NavigationBar";
+import Script from "next/script";
 import { Props } from "../interfaces";
 
 function PageLayout({ children }: Props) {
@@ -12,36 +13,27 @@ function PageLayout({ children }: Props) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <script src="/assets/js/jquery-3.5.1.min.js" type="text/javascript" />
-        <script src="/assets/js/popper.min.js" type="text/javascript" />
-        <script src="/assets/js/bootstrap.min.js" type="text/javascript" />
-
-        <script src="/assets/js/form-validator.min.js" type="text/javascript" />
-
-        <script
-          src="/assets/js/contact-form-script.js"
-          type="text/javascript"
-        />
-        <script
-          src="/assets/js/jquery.ajaxchimp.min.js"
-          type="text/javascript"
-        />
-        <script src="/assets/js/jquery.meanmenu.js" type="text/javascript" />
-        <script src="/assets/js/owl.carousel.min.js" type="text/javascript" />
-        <script src="/assets/js/wow.min.js" type="text/javascript" />
-        <script src="/assets/js/odometer.min.js" type="text/javascript" />
-        <script src="/assets/js/jquery.appear.min.js" type="text/javascript" />
-        <script
-          src="/assets/js/jquery.nice-select.min.js"
-          type="text/javascript"
-        />
-        <script
-          src="/assets/js/jquery.magnific-popup.min.js"
-          type="text/javascript"
-        />
-        <script src="/assets/js/custom.js" type="text/javascript" />
       </Head>
+      <Script src="/assets/js/jquery-3.5.1.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/popper.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/bootstrap.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/form-validator.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/contact-form-script.js" strategy="lazyOnload" />
+      <Script src="/assets/js/jquery.ajaxchimp.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/jquery.meanmenu.js" strategy="lazyOnload" />
+      <Script src="/assets/js/owl.carousel.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/wow.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/odometer.min.js" strategy="lazyOnload" />
+      <Script src="/assets/js/jquery.appear.min.js" strategy="lazyOnload" />
+      <Script
+        src="/assets/js/jquery.nice-select.min.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="/assets/js/jquery.magnific-popup.min.js"
+        strategy="lazyOnload"
+      />
+      <Script src="/assets/js/custom.js" strategy="lazyOnload" />
       <HeaderArea />
       <NavigationBar />
       {children}
