d87745a58f9c1f4bbed9329f4af53508be9456d3
aguchux <agu.chux@yahoo.com>
2022-09-19 08:38:06 +0100

    Investments

--- pages/api/users/info.ts
+++ pages/api/users/info.ts
@@ -12,8 +12,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         },
         POST: async (req: NextApiRequest, res: NextApiResponse) => {
             const { token } = req.body;
-            const { Account } = await dbCon();
-            const account = await Account.findOne({ _id: token }).catch(catcher);
+            const { Accounts } = await dbCon();
+            const account = await Accounts.findOne({ _id: token }).catch(catcher);
             if (account) {
                 res.status(200).json({
                     status: 1,
